8e623b7752288296c5885689c56ab97e758ad1e3
Youngmin Cho <choyoungmin0322@gmail.com>
2025-02-04 23:04:45 +0900

    Initial Commit

---
+++ MyFourCut/MyFourCutApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyFourCutApp.swift
+//  MyFourCut
+//
+//  Created by 조영민 on 2/4/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyFourCutApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
