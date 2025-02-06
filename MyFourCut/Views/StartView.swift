3bef6de13ae33dc9be71419f8343257b23d23ef8
Youngmin Cho <choyoungmin0322@gmail.com>
2025-02-06 22:43:13 +0900

    시작 뷰 파일 추가 및 4컷 이미지 삭제 버튼 구현

---
+++ MyFourCut/Views/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  MyFourCut
+//
+//  Created by 조영민 on 2/6/25.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
