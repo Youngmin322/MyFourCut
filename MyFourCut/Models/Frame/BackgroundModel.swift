add9e512872ead1c1c41b666ff5e706f0bd50277
Youngmin Cho <choyoungmin0322@gmail.com>
2025-07-16 18:07:15 +0900

    Feat: 프레임 추가 기능 도입을 위한 BackgroundModel 구조 변경

--- MyFourCut/Models/Frame/BackgroundModel.swift
+++ MyFourCut/Models/Frame/BackgroundModel.swift
@@ -6,11 +6,37 @@
 //
 
 import Foundation
+import UIKit
 
 struct BackgroundModel: Identifiable, Equatable {
     let id: String
-    let imageName: String
+    let imageName: String?  // 기본 번들 이미지용
     let displayName: String
+    let customImage: UIImage?  // 사용자 커스텀 이미지용
+    let isCustom: Bool
+    
+    // 기존 생성자
+    init(id: String, imageName: String, displayName: String) {
+        self.id = id
+        self.imageName = imageName
+        self.displayName = displayName
+        self.customImage = nil
+        self.isCustom = false
+    }
+    
+    // 커스텀 이미지용 생성자
+    init(id: String, customImage: UIImage, displayName: String) {
+        self.id = id
+        self.imageName = nil
+        self.displayName = displayName
+        self.customImage = customImage
+        self.isCustom = true
+    }
+    
+    // Equatable 프로토콜 준수
+    static func == (lhs: BackgroundModel, rhs: BackgroundModel) -> Bool {
+        return lhs.id == rhs.id
+    }
     
     static let defaultBackgrounds = [
         BackgroundModel(id: "bg0", imageName: "bg0", displayName: "기본"),
